ecef937028ceb4f9d1ea7134ec9316c662eeeb39
Richard Anaya <richard.anaya@gmail.com>
2019-04-23 05:27:35 +0000

    readme

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# ECS
+# ECS.jl
+
+An entity component system written for Julia
+
+```julia
+mutable struct Position <: ECS.Component
+  x::Float32
+  y::Float32
+end
+
+mutable struct Velocity <: ECS.Component
+  x::Float32
+  y::Float32
+end
+
+# Create a world for entities
+world = ECS.create()
+
+# Register memory storage for components
+ECS.register(world,Position)
+ECS.register(world,Velocity)
+
+# Create entities
+player = ECS.createentity!(world)
+ECS.addcomponent!(world,player,Position(0,0))
+ECS.addcomponent!(world,player,Velocity(1,2))
+
+# Run systems on entities with specific sets of components
+ECS.runsystem!(world,[Position,Velocity]) do entity,components 
+    # Components are given in order they are requested
+    pos,vel = components
+    # Modify components
+    pos.x += vel.x
+    pos.y += vel.y
+end
+```
--- src/ECS.jl
+++ src/ECS.jl
@@ -1,5 +1,107 @@
 module ECS
 
-greet() = print("Hello World!")
+abstract type Component end
+mutable struct World
+    entity_generation::Array{Union{Int64,Nothing}}
+    max_entity::Int64
+    free_entities::Array{Int64,1}
+    components::Dict{DataType,Any}
+end
 
-end # module
+struct EntityKey
+    index::Int64
+    generation::Int64
+end
+
+function World()
+    World(Array{Union{Int64,Nothing}}(undef,100),0,[],Dict())
+end
+
+function getentity(world::World,key::EntityKey)
+    current_generation = world.entity_generation[key.index]
+    if current_generation == nothing || current_generation != key.generation
+        return nothing
+    end
+    key.index
+end
+
+function register!(world::World,::Type{C}) where C <: Component
+    world.components[C] = Array{Union{C,Nothing}}(undef,1000)
+    nothing
+end
+
+function destroyentity!(world::World,entity::EntityKey)
+    if getentity(world,entity) == nothing
+        println("tried to destroy non existant entity")
+        return
+    end
+    for x in keys(world.components)
+        world.components[x][entity.index] = nothing
+    end
+    push!(world.free_entities,entity.index)
+    # makes sure entity cant be retrieved again
+    world.entity_generation[entity.index] += 1
+    nothing
+end
+
+function createentity!(world::World)
+    i = world.max_entity += 1
+    current_generation = world.entity_generation[i]
+    if current_generation == nothing
+        current_generation = 1
+    else
+        current_generation += 1
+    end
+    world.entity_generation[i] = current_generation
+    # clear out components for the new entity
+    for x in keys(world.components)
+        componentList = world.components[x]
+        if i > length(componentList)
+            resize!(componentList,2*length(componentList))
+        end
+        componentList[i] = nothing
+    end
+    EntityKey(i,current_generation)
+end
+
+function addcomponent!(world::World,entity::EntityKey,component::C) where C <: Component
+    if getentity(world,entity) == nothing
+        println("tried to add component to non existant entity")
+        return
+    end
+    world.components[C][entity.index] = component
+    nothing
+end
+
+function removecomponent!(world::World,entity::EntityKey,::Type{C}) where C <: Component
+    if getentity(world,entity) == nothing
+        println("tried to remove component to non existant entity")
+        return
+    end
+    world.components[C][entity.index] = nothing
+    nothing
+end
+
+function runsystem!(f,world::World,types::Array{DataType,1})
+    num_types = length(types)
+    for e = 1:world.max_entity
+        components = []
+        for t in types
+            component_list = world.components[t]
+            if e<=length(component_list) && component_list[e] != nothing
+                # if we found a component, store it
+                push!(components,component_list[e])
+            else
+                # if we find nothing we can stop right now
+                break
+            end
+        end
+        # if entity has all components call function
+        if length(components) == num_types
+            f(EntityKey(e,world.entity_generation[e]),components)
+        end
+    end
+    nothing
+end
+
+end
